3f419fedb05e036c5b70b9b00c1d1447d98e28a3
ProfSathishR <30977778+Professor-Sathish@users.noreply.github.com>
2024-04-06 16:30:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ from joblib import load
 import pandas as pd
 
 # Load the XGBoost model
-xgboost_model = load('xgboost_model.joblib')
+xgboost_model = load('car_price_predictor')
 
 # Define a function to preprocess input data
 def preprocess_input(data):
